0807f9bd88f89b6b937709cdcde6ade253fda39a
cheers8oys <cheers8oys@gmail.com>
2025-10-24 14:00:18 +0900

    refactor(CarTest): 테스트 효율을 위한 setUp 메서드 구현
    - 추후 기능 테스트 효율성을 위해 테스트 전 입력받을 자동차 객체 미리 생성

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -5,29 +5,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.view.InputView;
 
 public class CarTest {
 
-    @DisplayName("입력받은 자동차 등록 테스트")
-    @Test
-    public void testCarRegistration() {
+    private List<Car> cars;
 
-        String inputValue = "car1, car2";
+    @BeforeEach
+    public void setUp() {
+        String inputValue = "car1, car2\n";
         System.setIn(new ByteArrayInputStream(inputValue.getBytes()));
 
         InputView inputView = new InputView();
         List<String> carNames = inputView.readCarNames();
-        List<Car> cars = new ArrayList<>();
-        for(int i = 0; i < carNames.size(); i++){
+
+        cars = new ArrayList<>();
+        for (int i = 0; i < carNames.size(); i++) {
             cars.add(new Car(carNames.get(i)));
         }
+    }
 
+    @DisplayName("입력받은 자동차 등록 테스트")
+    @Test
+    public void testCarRegistration() {
         assertEquals(2, cars.size());
         assertEquals("car1", cars.get(0).getName());
         assertEquals("car2", cars.get(1).getName());
-
     }
 }
