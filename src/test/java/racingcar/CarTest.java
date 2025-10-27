6bda8b4ad4c339cfdbeed4ebb1495165934bdb96
cheers8oys <cheers8oys@gmail.com>
2025-10-27 12:26:05 +0900

    fix(CarTest): 리팩터링으로 인한 기존 테스트 코드 오류 해결

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -3,47 +3,30 @@ package racingcar;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.ByteArrayInputStream;
-import java.util.ArrayList;
 import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.model.Car;
 import racingcar.model.Cars;
-import racingcar.view.InputView;
 
 public class CarTest {
 
     private List<Car> cars;
 
-    @BeforeEach
-    public void setUp() {
-        String inputValue = "car1, car2\n";
-        System.setIn(new ByteArrayInputStream(inputValue.getBytes()));
-
-        InputView inputView = new InputView();
-        List<String> carNames = inputView.readCarNames();
-
-        cars = new ArrayList<>();
-        for (int i = 0; i < carNames.size(); i++) {
-            cars.add(new Car(carNames.get(i)));
-        }
-    }
-
     @DisplayName("입력받은 자동차 등록 테스트")
     @Test
     public void testCarRegistration() {
-        assertEquals(2, cars.size());
-        assertEquals("car1", cars.get(0).getName());
-        assertEquals("car2", cars.get(1).getName());
+        Cars cars = new Cars(List.of("car1", "car2"));
+        assertEquals("car1", cars.getCars().get(0).getName());
+        assertEquals("car2", cars.getCars().get(1).getName());
     }
 
     @DisplayName("자동차들의 현재 위치 확인 테스트")
     @Test
     public void testCheckingCarPosition() {
-        assertEquals(0, cars.get(0).getPosition());
-        assertEquals(0, cars.get(1).getPosition());
+        Cars cars = new Cars(List.of("pobi", "woni", "jun"));
+        assertEquals(0, cars.getCars().get(0).getPosition());
+        assertEquals(0, cars.getCars().get(1).getPosition());
     }
 
     @DisplayName("자동차 전진 기능 테스트")
